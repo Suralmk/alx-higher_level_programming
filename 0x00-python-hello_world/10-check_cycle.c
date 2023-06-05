29f42774dfc372eb0641cbc67fb1ff8c0ccc35a3
Suramk <surafelmelaku940@gmail.com>
2023-06-05 11:50:18 -0700

    check for cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,29 @@
 #include "lists.h"
+
 /**
- * check_cycle - checks if there exists a loop or cycle
- * @list: head of the linked list
- * Return: return 1 on success and 0 on fail
+ * check_cycle - checks for loop or cycle
+ * @list: head of linked list
+ * Return: 1 if there is cycle and 0 if not
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *first, *second;
+listint_t *first, *second;
 
-	if (list == NULL)
-	{
-		return (0);
-	}
-		first = list;
-		second = list->next;
-	for (; second && first && second->next;)
-	{
-		if (first == second)
-		{
-			return (1);
-		}
-		first = first->next;
-		second = second->next->next;
-	}
+if (list == NULL)
+{
+return (0);
+}
+first = list;
+second = list->next;
+for (; second && first && second->next;)
+{
+if (first == second)
+{
+return (1);
+}
+first = first->next;
+second = second->next->next;
+}
 return (0);
 }
