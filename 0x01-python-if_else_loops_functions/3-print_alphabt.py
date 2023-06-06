cc3c55ce95a4e5052e2e45d86d324a2b9a4c11a3
Suramk <surafelmelaku940@gmail.com>
2023-06-06 11:04:42 -0700

    hexa

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-i = ord('a')
-while (i < ord('z')):
+for i in range(ord('a'), ord('z') + 1):
     if (i != ord('e') and i != ord('q')):
         print('{:c}'.format(i), end='')
-    i = i + 1
