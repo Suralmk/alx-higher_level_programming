67bf2c54f47e1f668f3acf8fa70a109c90f63c00
Suramk <surafelmelaku940@gmail.com>
2023-06-12 07:04:46 -0700

    rackets added

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,6 +6,6 @@ def replace_in_list(my_list, idx, element):
     for i in my_list:
         if i == idx:
             my_list[i] = element
-            return my_list
+            return(my_list)
         elif idx > len(my_list) or idx < 0:
-            return my_list
+            return (my_list)
