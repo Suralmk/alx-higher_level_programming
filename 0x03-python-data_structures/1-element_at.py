f8f40e6374bad4b2dfa76d240f25b602d4c0ea25
Suramk <surafelmelaku940@gmail.com>
2023-06-12 07:00:49 -0700

    3

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    i = 0
+    for i in my_list:
+            if i == idx:
+                return my_list[i]
+            else:
+                continue
