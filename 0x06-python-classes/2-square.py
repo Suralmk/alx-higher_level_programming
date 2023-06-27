e334b1aec804defd36efaf53e0b71ee51029ce89
Suramk <surafelmelaku940@gmail.com>
2023-06-27 07:38:09 -0700

    nw

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,11 +7,13 @@ class Square:
 
     def __init__(self, size=0):
 
-        """instanciated the class with var          iable xize
+        """
+        instanciated the class with variable xize
         raise TypeError : if size is other than int
-        raise ValueError : if size is less than zero"""
+        raise ValueError : if size is less than zero
+        """
 
-        if not isinstance(self, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError(size must be >= 0)
