c106ff71c37347ce7ae08b8c7c3e70cae132f110
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-06-27 17:13:22 +0300

    py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """defining a class"""
+
 class Square:
     """class called square"""
+    pass
 
     def __init__(self, size):
         """square class initialized with size"""
