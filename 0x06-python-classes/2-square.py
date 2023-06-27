dbab510f17c8482ffd31b888626813b0e9d890dd
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-06-27 17:27:13 +0300

    validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defining class"""
+class Square:
+    """class defined"""
+
+    def __init__(self, size=0):
+
+        """instanciated the class with var          iable xize
+        raise TypeError : if size is other than int
+        raise ValueError : if size is less than zero"""
+
+        if not isinstance(self, size=0):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError(size must be >= 0)
+
+
+        self.__size = size
