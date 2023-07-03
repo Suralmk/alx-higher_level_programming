fef50ab6a74e293ac09cfa110863f33c25c26db0
Suramk <surafelmelaku940@gmail.com>
2023-07-03 06:34:22 -0700

    new

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-    def __str__(self):
+    def __str__(self) -> str :
         """Returns Printable form of the rectangle in #"""
         if self.__width == 0 or self.__height == 0:
             return ("")
