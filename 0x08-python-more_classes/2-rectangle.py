ce00e6970e8fdcd66c75a0c29acd4ce2f2d6410b
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-07-04 15:21:01 +0300

    new

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """defines rectangle"""
+    def __init__ (self, width=0, height=0):
+        """initialize a new rectangle
+        Args:
+        width(int): width of the new rctangle
+        height(int): height of the rectangle
+        """
+        self.__width = width
+        self.__height = heght
+
+    @property
+    def width(self):
+        """define width method to retrive  widdth"""
+        return self.__width
+
+    @propert_setter
+    def width(self, value):
+        """set width to value
+        Args:
+        value(int): the new width value
+        """
+
+        if isinstance(value, int) == False:
+
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """height mwthod to retrive height"""
+
+        return self.__height
+
+
+    @property_setter
+    def height(self, value):
+        """set height value to value variable"""
+
+        if isinstance(value, int) == False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(st=[]):
-    st += ["Holberton"]
-    return ", ".join(st)
